523459710c437d0950c9c53f17125ad9d7ae7c5c
Mondo <mondogao@gmail.com>
2018-06-10 16:30:06 +0800

    feat(SportStatus): finish ui

--- app/components/SportStatus/index.jsx
+++ app/components/SportStatus/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Timeline, Button } from 'antd';
 
 import styles from './SportStatus.scss';
 
@@ -16,20 +16,28 @@ export default class SportStatus extends React.Component {
   mountMap() {
     const amapIns = new AMap.Map(this.mapRef.current, {
       center: [114.431282, 30.512939],
-      zoom: 18,
+      zoom: 19,
+      zooms: [17, 19],
     });
     this.setState(() => ({
       amapIns,
     }));
   }
 
+  handleFinish = () => {
+    this.props.next();
+  };
+
   render() {
     return (
       <Layout className={styles.container}>
         <div className={styles.mapContainer} ref={this.mapRef} />
-        <div>
-
+        <div className={styles.timelineContainer}>
+          <Timeline pending="已跑 1 km, 用时 1 分">
+            <Timeline.Item color="green">开始时间: {1}</Timeline.Item>
+          </Timeline>
         </div>
+        <Button onClick={this.handleFinish}>结束运动</Button>
       </Layout>
     );
   }
