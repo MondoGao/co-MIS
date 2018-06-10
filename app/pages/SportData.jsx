e8add21760bc7c5491b64a42d069936b04490a27
Mondo <mondogao@gmail.com>
2018-06-10 17:53:37 +0800

    feat(SportTable): add table def

--- app/pages/SportData.jsx
+++ app/pages/SportData.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import SportTable from '@/components/SportTable';
+
+const { Fragment } = React;
+
 export default class SportData extends React.Component {
   render() {
-    return 'xxx';
+    return (
+      <Fragment>
+        <SportTable />
+      </Fragment>
+    )
   }
 }
