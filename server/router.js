cfdc301a4c54ce2a6f7de445e75d97cabad4b716
Mondo <mondogao@gmail.com>
2018-06-14 09:39:23 +0800

    feat(server/router): add login

--- server/router.js
+++ server/router.js
@@ -2,11 +2,32 @@ const KoaRouter = require('koa-router');
 const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa');
 
 const graphql = require('./graphql');
+const { User } = require('./models');
 
 const router = new KoaRouter();
 
 router.get('/', (ctx, next) => {
   ctx.body = 'hello koa';
+  next();
+});
+
+router.post('/login', async (ctx, next) => {
+  const { body } = ctx.request;
+  const user = await User.findOne({
+    rfid: body.rfid,
+  }).lean();
+
+  if (!user) {
+    ctx.throw(401, 'user not found', body);
+    next();
+    return;
+  }
+
+  ctx.cookies.set('id', user._id);
+
+  ctx.body = {
+    data: user,
+  };
   next();
 });
 
