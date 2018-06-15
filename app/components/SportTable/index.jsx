67e38e8388d7a8aba1dbb7c35fcb70803fde29d8
Mondo <mondogao@gmail.com>
2018-06-15 11:06:59 +0800

    feat: finish sportTable

--- app/components/SportTable/index.jsx
+++ app/components/SportTable/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Table, Button } from 'antd';
+import { DateTime } from 'luxon';
 
 import { sports } from '@/sources';
 
 const { Column, ColumnGroup } = Table;
+const { Fragment } = React;
 
 export default class SportTable extends React.Component {
   state = {
@@ -20,7 +22,7 @@ export default class SportTable extends React.Component {
       isLoading: true,
     });
 
-    const newData = await sports.all();
+    const newData = await sports.queryData();
     console.log(newData);
 
     this.setState({
@@ -31,22 +33,61 @@ export default class SportTable extends React.Component {
 
   render() {
     const { data, isLoading } = this.state;
+    const actons = (
+      <Column
+        title="操作"
+        key="action"
+        render={(text, record) => (
+          <span>
+            <Button>查看路径</Button>
+          </span>
+        )}
+      />
+    );
 
     return (
-      <Table rowKey="id" bordered dataSource={data} loading={isLoading}>
-        <Column title="运动编号" dataIndex="id" key="id" />
-        <Column title="开始时间" dataIndex="startTime" key="startTime" />
-        <Column title="是否结束" dataIndex="isFinished" key="isFinishes" />
-        <Column
-          title="操作"
-          key="action"
-          render={(text, record) => (
-            <span>
-              <Button>查看路径</Button>
-            </span>
-          )}
-        />
-      </Table>
+      <Fragment>
+        <Button onClick={this.loadData}>刷新</Button>
+        <Table rowKey="id" bordered dataSource={data} loading={isLoading}>
+          <Column title="运动编号" dataIndex="id" key="id" />
+          <Column
+            title="用户名"
+            dataIndex="user"
+            key="userName"
+            render={user => user.name}
+          />
+          <Column
+            title="追踪器编号"
+            dataIndex="tracker"
+            key="trackerChannel"
+            render={tracker => tracker.channel}
+          />
+          <Column
+            title="开始时间"
+            dataIndex="startTime"
+            key="startTime"
+            render={text => {
+              return DateTime.fromISO(text).toLocaleString(
+                DateTime.DATETIME_SHORT_WITH_SECONDS,
+              );
+            }}
+          />
+          <Column
+            title="结束时间"
+            dataIndex="endTime"
+            key="endTime"
+            render={text => {
+              if (!text) {
+                return '未结束';
+              }
+
+              return DateTime.fromISO(text).toLocaleString(
+                DateTime.DATETIME_SHORT_WITH_SECONDS,
+              );
+            }}
+          />
+        </Table>
+      </Fragment>
     );
   }
 }
