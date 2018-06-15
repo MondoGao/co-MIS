4c2de9c732ab4a081230fbee985e5f8f037ab934
Mondo <mondogao@gmail.com>
2018-06-15 14:31:31 +0800

    feat(EquipData): add filters

--- app/pages/EquipData.jsx
+++ app/pages/EquipData.jsx
@@ -39,6 +39,24 @@ export default class EquipData extends React.Component {
       return `${hour} 时 ${minute} 分`;
     }
 
+    const rfidFilters = R.uniq(data.map(({ rfid }) => rfid)).map(name => {
+      if (name == null) {
+        return {
+          text: '无标签',
+          value: 'none',
+        };
+      }
+      return {
+        text: name,
+        value: name,
+      };
+    });
+
+    const typeFilters = R.uniq(data.map(({ type }) => type.name)).map(name => ({
+      text: name,
+      value: name,
+    }));
+
     return (
       <Fragment>
         <Button loading={isLoading} onClick={this.loadData}>
@@ -48,12 +66,30 @@ export default class EquipData extends React.Component {
         <Table rowKey="id" bordered dataSource={data} loading={isLoading}>
           <Column title="器材编号" dataIndex="id" key="id" />
           <Column title="名称" dataIndex="name" key="name" />
-          <Column title="类型" dataIndex="type.name" key="type" />
+          <Column
+            title="类型"
+            dataIndex="type.name"
+            key="type"
+            filters={typeFilters}
+            onFilter={(value, record) => record.type.name.indexOf(value) === 0}
+          />
           <Column
             title="电子标签编号"
             dataIndex="rfid"
             key="rfid"
             render={text => (text ? text : '无')}
+            filters={rfidFilters}
+            onFilter={(value, record) => {
+              if (value === 'none') {
+                return record.rfid == null;
+              }
+
+              if (record.rfid == null) {
+                return false;
+              }
+
+              return record.rfid.indexOf(value) === 0;
+            }}
           />
           <ColumnGroup title="可用时间">
             <Column
